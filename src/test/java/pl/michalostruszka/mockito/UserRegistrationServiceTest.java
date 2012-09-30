a27df48b1a7ab2b14c9f9f986080dcf5c2a174d1
Michal Ostruszka <michal.ostruszka@gmail.com>
2012-09-30 12:22:20 +0200

    correct implementation, all tests passing with mock() and no runner

--- src/test/java/pl/michalostruszka/mockito/UserRegistrationServiceTest.java
+++ src/test/java/pl/michalostruszka/mockito/UserRegistrationServiceTest.java
@@ -2,23 +2,20 @@ package pl.michalostruszka.mockito;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.*;
 
 /**
  * Author: michal
  */
-@RunWith(MockitoJUnitRunner.class)
 public class UserRegistrationServiceTest {
 
-    @Mock private UsersRepository usersRepository;
+    private UsersRepository usersRepository;
     private UserRegistrationService registrationService;
 
     @Before
     public void setUp() throws Exception {
+        usersRepository = mock(UsersRepository.class);
         registrationService = new UserRegistrationService(usersRepository);
     }
 
